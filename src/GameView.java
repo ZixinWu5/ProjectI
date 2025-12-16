f0f3489aa5c96f2355160d1f7e6e6e841ea84328
ZixinWu5 <zwu002@student.sdccd.edu>
2025-12-15 20:06:14 -0800

    yeah,changes

--- src/GameView.java
+++ src/GameView.java
@@ -11,6 +11,18 @@
  *         version 1
  */
 
+/**
+ * Lead Author(s):
+ *
+ * @author 5550129061; zixin wu
+ *         References:
+ *         Morelli, R., & Walde, R. (2016). Java, Java, Java: Object-Oriented
+ *         Problem Solving.
+ *         Retrieved from
+ *         https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+ *         version 1
+ */
+
 import java.util.List;
 
 public interface GameView
